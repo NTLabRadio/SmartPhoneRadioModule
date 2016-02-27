1c36b394e6e647014ec2c972c779d1f12e6cab2a
Pavel Moroshek <p_moris@tut.by>
2016-02-27 18:14:57 +0300

    1

---
+++ Inc/AD5601.h
@@ -0,0 +1,29 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SPI_AD5601_H
+#define __SPI_AD5601_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32f0xx_hal.h"
+#include "stm32f0xx_hal_spi.h"
+#include "spi_periphery.h"
+#include "timers.h"
+	 
+	 
+
+
+
+
+
+	 
+	 
+uint8_t DAC_write (SPI_HandleTypeDef *hspi);	 
+	 
+	 
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __SPI_AD5601_H */	 
